eb7b9591086bdbb8e9b2212bc78c52f1cc95b1a0
Adam Calhoun and Samuel Gespass <adam@>
2017-12-04 13:05:11 -0800

    change scripts to use filter method

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,15 @@ $(document).ready(function(){
   $("form#theForms").submit(function(event){
     event.preventDefault();
     var play = $("#sentence").val();
-    var result =[];
+    var result = [];
     words = play.split(' ');
 
-    words.forEach(function(word){
-      if (word.length >=3){
-        result.push(word);
-      }
-    });
+    // words.forEach(function(word){
+    //   if (word.length >=3){
+    //     result.push(word);
+    //   }
+    // });
+    result = words.filter(word => word.length >= 3);
 
     console.log(result.join(' '));
   });
